a1436a39688a3a0f7f088498a342a57058adb28e
luan-services <luandotservices@gmail.com>
2025-06-01 21:24:12 -0300

    cursor

--- components/SocialBtn.jsx
+++ components/SocialBtn.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 const SocialBtn = (props) => {
   return (
     <button className="p-3 w-full rounded-xl bg-custom-btn 0 text-sm font-bold
-        hover:bg-custom-color-green hover:text-gray-900 hover:transition hover:duration-400
+        hover:bg-custom-color-green hover:text-gray-900 hover:transition hover:duration-400 hover:cursor-pointer
     active:scale-95 active:brightness-80"> 
         <span>{props.children != null ? props.children : props.text}</span>
         {/* aqui a props.children é o texto dentro dos <>'txt'</> do componente */}
